4a69700b297872ba0c5bff3c5296fa3bd5a81423
yukino <yukino161043261@gmail.com>
2024-05-23 00:07:27 +0800

    Update test.py to support nagrs, sample: `python3 test.py -e core 1B earth`

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 #
-# Created by TianchengHang on 2024/5/8
+# Created by Tiancheng Hang on 2024/5/8
 #
 import argparse
 import os
-import math
 
 if __name__ == "__main__":
     print("=" * 20)
@@ -16,30 +15,26 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-a",
-        "--all",
-        action="store_true",
-        help="execute all experiments, default false",
-    )
-
-    parser.add_argument(
         "-e",
         "--exp",
-        default="core",
+        nargs="+",
+        default=["core"],
         type=str,
-        choices=["core", "1B", "earth", "MT", "Para", "Seg", "WPMT", "zipf"],
-        help="which experiment, default 'core'",
-    )
-
-    # Command "python3 test.py -e 1B" specifies `./eva-scripts/run_1B.py` on `graphs_0`, `graphs_1`, `graphs_2`
-    # Type "python3 test.py --help" to show help message
+        choices=["core", "1B", "earth", "MT", "Para", "Seg", "WPMT", "zipf", "all"],
+        help="""
+        which experiment, default 'core'. 
+        When core is specified, perform 'core' experiments.
+        When all is specified, perform ALL experiments.
+        """
+    )  # Type "python3 test.py --help" to show help message
 
     args = parser.parse_args()
-    if args.all:  # execute all experiments
+    if args.exp.__contains__("all"):  # execute ALL experiments
         exe = input("Execute all experiments, y/n? ")
         if exe != "y" and exe != "Y":
             exit(0)
         for exp in ("core", "1B", "earth", "MT", "Para", "Seg", "WPMT", "zipf"):
+            # print(f"python3 eva-scripts/run_{exp}.py")
             os.system(f"python3 eva-scripts/run_{exp}.py")
             for trial in (0, 1, 2):
                 print(
@@ -49,17 +44,21 @@ if __name__ == "__main__":
                     + "=" * 50
                     + "\033[0m"
                 )
+                # print(f"eva-scripts/gen_graphs_{exp}.sh graphs_{trial}")
                 os.system(f"eva-scripts/gen_graphs_{exp}.sh graphs_{trial}")
     else:
-        os.system(f"python3 eva-scripts/run_{args.exp}.py")
-        for trial in (0, 1, 2):
-            print(
-                "\033[32m"
-                + "=" * 50
-                + f"Experiment {args.exp} on graphs_{trial}"
-                + "=" * 50
-                + "\033[0m"
-            )
-            os.system(f"eva-scripts/gen_graphs_{args.exp}.sh graphs_{trial}")
+        for exp in args.exp:
+            # print(f"python3 eva-scripts/run_{exp}.py")
+            os.system(f"python3 eva-scripts/run_{exp}.py")
+            for trial in (0, 1, 2):
+                print(
+                    "\033[32m"
+                    + "=" * 50
+                    + f"Experiment {exp} on graphs_{trial}"
+                    + "=" * 50
+                    + "\033[0m"
+                )
+                # print(f"eva-scripts/gen_graphs_{exp}.sh graphs_{trial}")
+                os.system(f"eva-scripts/gen_graphs_{exp}.sh graphs_{trial}")
 
     print("Done!")
