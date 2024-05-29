e971075ef0cd6bed23044b56e0412839dfacbb25
yukino <yukino161043261@gmail.com>
2024-05-29 22:49:12 +0800

    Update shell scripts

--- plot.sh
+++ plot.sh
@@ -2,65 +2,127 @@
 
 chmod -R 755 eva-scripts
 chmod -R 755 gnuplot-scripts
-
-if [ -z $1 ]
-then
-    ./eva-scripts/gen_graphs_core.sh graphs_0
-    ./eva-scripts/gen_graphs_core.sh graphs_1
-    ./eva-scripts/gen_graphs_core.sh graphs_2
-    exit
-fi
+mkdir -p ./output
 
 # core
-if [ $1 == "core" ] || [ $1 == "all" ]
+if [ -z $1 ] || [ $1 == "core" ]
 then
-    ./eva-scripts/gen_graphs_core.sh graphs_0
-    ./eva-scripts/gen_graphs_core.sh graphs_1
-    ./eva-scripts/gen_graphs_core.sh graphs_2
+    rm -rf ./output/plot_core
+    mkdir  ./output/plot_core
+    ./eva-scripts/gen_graphs_core.sh graphs_0 &> ./output/plot_core/graphs_0.output
+    ./eva-scripts/gen_graphs_core.sh graphs_1 &> ./output/plot_core/graphs_1.output
+    ./eva-scripts/gen_graphs_core.sh graphs_2 &> ./output/plot_core/graphs_2.output
 
 # 1B
-elif [ $1 == "1B" ] || [ $1 == "all" ]
+elif [ $1 == "1B" ]
 then
-    ./eva-scripts/gen_graphs_1B.sh graphs_1B_0
-    ./eva-scripts/gen_graphs_1B.sh graphs_1B_1
+    rm -rf ./output/plot_1B
+    mkdir  ./output/plot_1B
+    ./eva-scripts/gen_graphs_1B.sh graphs_1B_0 &> ./output/plot_1B/graphs_1B_0.output
+    ./eva-scripts/gen_graphs_1B.sh graphs_1B_1 &> ./output/plot_1B/graphs_1B_1.output
 
 # earth
-elif [ $1 == "earth" ] || [ $1 == "all" ]
+elif [ $1 == "earth" ]
 then
-    ./eva-scripts/gen_graphs_earth.sh graphs_earth_0
-    ./eva-scripts/gen_graphs_earth.sh graphs_earth_1
+    rm -rf ./output/plot_earth
+    mkdir  ./output/plot_earth
+    ./eva-scripts/gen_graphs_earth.sh graphs_earth_0 &> ./output/plot_earth/graphs_earth_0.output
+    ./eva-scripts/gen_graphs_earth.sh graphs_earth_1 &> ./output/plot_earth/graphs_earth_1.output
 
 # MT
-elif [ $1 == "MT" ] || [ $1 == "all" ]
+elif [ $1 == "MT" ]
 then
-    ./eva-scripts/gen_graphs_MT.sh graphs_MT_0
-    ./eva-scripts/gen_graphs_MT.sh graphs_MT_1
+    rm -rf ./output/plot_MT
+    mkdir  ./output/plot_MT
+    ./eva-scripts/gen_graphs_MT.sh graphs_MT_0 &> ./output/plot_MT/graphs_MT_0.output
+    ./eva-scripts/gen_graphs_MT.sh graphs_MT_1 &> ./output/plot_MT/graphs_MT_1.output
 
 # Para
-elif [ $1 == "Para" ] || [ $1 == "all" ]
+elif [ $1 == "Para" ]
 then
-    ./eva-scripts/gen_graphs_Para.sh graphs_Para_0
-    ./eva-scripts/gen_graphs_Para.sh graphs_Para_1
+    rm -rf ./output/plot_Para
+    mkdir  ./output/plot_Para
+    ./eva-scripts/gen_graphs_Para.sh graphs_Para_0 &> ./output/plot_Para/graphs_Para_0.output
+    ./eva-scripts/gen_graphs_Para.sh graphs_Para_1 &> ./output/plot_Para/graphs_Para_1.output
 
 # Seg
-elif [ $1 == "Seg" ] || [ $1 == "all" ]
+elif [ $1 == "Seg" ]
 then
-    ./eva-scripts/gen_graphs_Seg.sh graphs_Seg_0
-    ./eva-scripts/gen_graphs_Seg.sh graphs_Seg_1
+    rm -rf ./output/plot_Seg
+    mkdir  ./output/plot_Seg
+    ./eva-scripts/gen_graphs_Seg.sh graphs_Seg_0 &> ./output/plot_Seg/graphs_Seg_0.output
+    ./eva-scripts/gen_graphs_Seg.sh graphs_Seg_1 &> ./output/plot_Seg/graphs_Seg_1.output
 
 # WPMT
-elif [ $1 == "WPMT" ] || [ $1 == "all" ]
+elif [ $1 == "WPMT" ]
 then
-    ./eva-scripts/gen_graphs_WPMT.sh graphs_WPMT_0
-    ./eva-scripts/gen_graphs_WPMT.sh graphs_WPMT_1
-    ./eva-scripts/gen_graphs_WPMT.sh graphs_WPMT_2
+    rm -rf ./output/plot_WPMT
+    mkdir  ./output/plot_WPMT
+    ./eva-scripts/gen_graphs_WPMT.sh graphs_WPMT_0 &> ./output/plot_WPMT/graphs_WPMT_0.output
+    ./eva-scripts/gen_graphs_WPMT.sh graphs_WPMT_1 &> ./output/plot_WPMT/graphs_WPMT_1.output
+    ./eva-scripts/gen_graphs_WPMT.sh graphs_WPMT_2 &> ./output/plot_WPMT/graphs_WPMT_2.output
 
 # zipf
-elif [ $1 == "zipf" ] || [ $1 == "all" ]
+elif [ $1 == "zipf" ]
 then
-    ./eva-scripts/gen_graphs_zipf.sh graphs_zipf_0
-    ./eva-scripts/gen_graphs_zipf.sh graphs_zipf_1
-    ./eva-scripts/gen_graphs_zipf.sh graphs_zipf_2
+    rm -rf ./output/plot_zipf
+    mkdir  ./output/plot_zipf
+    ./eva-scripts/gen_graphs_zipf.sh graphs_zipf_0 &> ./output/plot_zipf/graphs_zipf_0.output
+    ./eva-scripts/gen_graphs_zipf.sh graphs_zipf_1 &> ./output/plot_zipf/graphs_zipf_1.output
+    ./eva-scripts/gen_graphs_zipf.sh graphs_zipf_2 &> ./output/plot_zipf/graphs_zipf_2.output
+
+elif [ $1 == "all" ]
+then
+    # core
+    rm -rf ./output/plot_core
+    mkdir  ./output/plot_core
+    ./eva-scripts/gen_graphs_core.sh graphs_0 &> ./output/plot_core/graphs_0.output
+    ./eva-scripts/gen_graphs_core.sh graphs_1 &> ./output/plot_core/graphs_1.output
+    ./eva-scripts/gen_graphs_core.sh graphs_2 &> ./output/plot_core/graphs_2.output
+
+    # 1B
+    rm -rf ./output/plot_1B
+    mkdir  ./output/plot_1B
+    ./eva-scripts/gen_graphs_1B.sh graphs_1B_0 &> ./output/plot_1B/graphs_1B_0.output
+    ./eva-scripts/gen_graphs_1B.sh graphs_1B_1 &> ./output/plot_1B/graphs_1B_1.output
+
+    # earth
+    rm -rf ./output/plot_earth
+    mkdir  ./output/plot_earth
+    ./eva-scripts/gen_graphs_earth.sh graphs_earth_0 &> ./output/plot_earth/graphs_earth_0.output
+    ./eva-scripts/gen_graphs_earth.sh graphs_earth_1 &> ./output/plot_earth/graphs_earth_1.output
+
+    # MT
+    rm -rf ./output/plot_MT
+    mkdir  ./output/plot_MT
+    ./eva-scripts/gen_graphs_MT.sh graphs_MT_0 &> ./output/plot_MT/graphs_MT_0.output
+    ./eva-scripts/gen_graphs_MT.sh graphs_MT_1 &> ./output/plot_MT/graphs_MT_1.output
+
+    # Para
+    rm -rf ./output/plot_Para
+    mkdir  ./output/plot_Para
+    ./eva-scripts/gen_graphs_Para.sh graphs_Para_0 &> ./output/plot_Para/graphs_Para_0.output
+    ./eva-scripts/gen_graphs_Para.sh graphs_Para_1 &> ./output/plot_Para/graphs_Para_1.output
+
+    # Seg
+    rm -rf ./output/plot_Seg
+    mkdir  ./output/plot_Seg
+    ./eva-scripts/gen_graphs_Seg.sh graphs_Seg_0 &> ./output/plot_Seg/graphs_Seg_0.output
+    ./eva-scripts/gen_graphs_Seg.sh graphs_Seg_1 &> ./output/plot_Seg/graphs_Seg_1.output
+
+    # WPMT
+    rm -rf ./output/plot_WPMT
+    mkdir  ./output/plot_WPMT
+    ./eva-scripts/gen_graphs_WPMT.sh graphs_WPMT_0 &> ./output/plot_WPMT/graphs_WPMT_0.output
+    ./eva-scripts/gen_graphs_WPMT.sh graphs_WPMT_1 &> ./output/plot_WPMT/graphs_WPMT_1.output
+    ./eva-scripts/gen_graphs_WPMT.sh graphs_WPMT_2 &> ./output/plot_WPMT/graphs_WPMT_2.output
+
+    # zipf
+    rm -rf ./output/plot_zipf
+    mkdir  ./output/plot_zipf
+    ./eva-scripts/gen_graphs_zipf.sh graphs_zipf_0 &> ./output/plot_zipf/graphs_zipf_0.output
+    ./eva-scripts/gen_graphs_zipf.sh graphs_zipf_1 &> ./output/plot_zipf/graphs_zipf_1.output
+    ./eva-scripts/gen_graphs_zipf.sh graphs_zipf_2 &> ./output/plot_zipf/graphs_zipf_2.output
 
 else
     echo "Please specify the correct expriment!"
--- run.sh
+++ run.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+rm -rf ./output
+mkdir ./output
+
 if [ -z $1 ]
 then
-    python3 ./eva-scripts/run_core.py
+    python3 ./eva-scripts/run_core.py  &> ./output/run_core.output
     exit
 fi
 
 if [ $1 == "all" ]
 then
-    python3 ./eva-scripts/run_core.py
-    python3 ./eva-scripts/run_1B.py
-    python3 ./eva-scripts/run_earth.py
-    python3 ./eva-scripts/run_MT.py
-    python3 ./eva-scripts/run_Para.py
-    python3 ./eva-scripts/run_Seg.py
-    python3 ./eva-scripts/run_WPMT.py
-    python3 ./eva-scripts/run_zipf.py
+    python3 ./eva-scripts/run_core.py  &> ./output/run_core.output
+    python3 ./eva-scripts/run_1B.py    &> ./output/run_1B.output
+    python3 ./eva-scripts/run_earth.py &> ./output/run_earth.output
+    python3 ./eva-scripts/run_MT.py    &> ./output/run_MT.output
+    python3 ./eva-scripts/run_Para.py  &> ./output/run_Para.output
+    python3 ./eva-scripts/run_Seg.py   &> ./output/run_Seg.output
+    python3 ./eva-scripts/run_WPMT.py  &> ./output/run_WPMT.output
+    python3 ./eva-scripts/run_zipf.py  &> ./output/run_zipf.output
 
 elif [ -a ./eva-scripts/run_$1.py ]
 then
